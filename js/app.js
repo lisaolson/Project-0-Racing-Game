e821d9e654dc244223831e4ac5e7f4006a1befd6
Lisa Olson <olson.lisa94@gmail.com>
2016-08-28 17:12:07 -0700

    Adds alert at finish line and background image

--- js/app.js
+++ js/app.js
@@ -3,11 +3,45 @@ console.log("JavaScript is connected!");
 
 //A = 65 L = 76 spacebar = 32
  //
+
+ // window.onLoad = function() {
+var finishLine = 1000;
+var startLine = 10;
+var $player1 = $('#player1');
+var $player2 = $('#player2');
+
+$('.btn-primary').on('click', function(event) {
+  document.location.reload(true);
+  })
+
  $(document).keydown(function(e) {
-    if (e.keyCode == 65) {
-      $('#player1').css('left', '+=10');
-    }
-    if(e.keyCode == 76 ) {
+
+      if (e.keyCode == 65) {
+          $('#player1').css('left', '+=10');
+          checkPosition($player1);
+          }
+
+      if(e.keyCode == 76 ) {
       $('#player2').css('left', '+=10');
-    }
+      checkPosition($player2);
+      }
  });
+//
+
+function checkPosition($player) {
+  console.log((parseInt($player.css('left'), 10)))
+  if (parseInt($player.css('left'), 10) > finishLine) {
+
+    alert("Yay! Crossed finish line!");
+  };
+}
+
+//  }
+// document.addEventListener('keydown', stopDivs)
+ // }
+//
+
+//
+//    }
+//  }
+// }
